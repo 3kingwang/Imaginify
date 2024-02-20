6066f6da18b738b8fdc05df971e1f48b6558045c
3king <wangxinnlg@126.com>
2024-02-20 14:50:07 +0800

    first commit

---
+++ app/(root)/transformations/[id]/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {
+  params: { id: string };
+};
+
+const TransformationsPage = ({ params: { id } }: Props) => {
+  return <div>Trans</div>;
+};
+
+export default TransformationsPage;
---
+++ lib/database/models/user.model.ts
@@ -0,0 +1,23 @@
+import { Schema, model, models } from "mongoose";
+
+const userSchema = new Schema({
+  clerkId: { type: String, required: true, unique: true },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  firstName: {
+    type: String,
+  },
+  lastName: {
+    type: String,
+  },
+  email: { type: String, required: true, unique: true },
+  photo: { type: String, required: true },
+  planId: { type: Number, default: 1 },
+  creditBalance: { type: Number, default: 10 },
+});
+
+const User = models.User || model("User", userSchema);
+export default User;
